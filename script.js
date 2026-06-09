22d2d65f528942a9cfa8758a8e080d8a2d732973
biancadranka-sketch <bianca.dranka@escola.pr.gov.br>
2026-06-09 11:37:02 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+//JS
+
+// =====================================================
+// ARQUIVO: script.js
+// Interações do site Agro Sustentável
+// =====================================================
+
+function mostrarMensagem() {
+
+    alert("🌱 O Agro Sustentável ajuda a produzir alimentos preservando o meio ambiente!");
+
+}
